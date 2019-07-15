718e1fce6538271d8a297320ac6a41764c218a54
Trevahr Robertson <Trevahr@gmail.com>
2019-07-15 12:51:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 class Application < Sinatra::Base
   # Write your code here!
 
+  get '/' do
+
+    erb :index
+  end
+
+  # get '/greet/:param_value' do
+  #   params[:param_value]
+  #     erb :greet
+  # end
+  post '/greet' do
+      erb :greet
+  end
 end
